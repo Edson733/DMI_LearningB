088b88821f7a3b55c4404337c836356737365d59
Edson733 <20213tn034@utez.edu.mx>
2024-10-02 20:19:36 -0600

    Perfil

--- lib/navigation/profile.dart
+++ lib/navigation/profile.dart
@@ -7,9 +7,66 @@ class Profile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Perfil'),),
-      body: const Center(
-        child: Text('Perfil')
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.black,
+                    foregroundColor: Colors.white,
+                    child: Text('EP'),
+                  ),
+                  SizedBox(width: 10),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start, 
+                    children: [
+                      Text(
+                        'Edson Miguel Peralta Valdez',
+                        style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                      ), 
+                      Text(
+                        'Hace 10 minutos',
+                        style: TextStyle(fontSize: 12, color: Colors.black54)
+                      )
+                    ]
+                  ),
+                  Spacer(),
+                  Icon(Icons.more_vert)
+                ],
+              ),
+            ),
+            Image.asset('cohete2.png', width: double.infinity, height: 400),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  Icon(Icons.favorite, color: Colors.red),
+                  Icon(Icons.message),
+                  Icon(Icons.ios_share),
+                  Spacer(),
+                  Icon(Icons.save_alt),
+                ],
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Datos del usuario'),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Datos del usuario'),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(16.0),
+              child: Text('Datos del usuario'),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
